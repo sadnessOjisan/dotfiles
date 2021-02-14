36d3190059dba4dc184b155a7434d095d37d537e
sadnessOjisan <sadness.ojisan@gmail.com>
2021-02-14 23:11:40 +0900

    tmux

--- fish/config.fish
+++ fish/config.fish
@@ -7,3 +7,24 @@ alias gia="git add"
 alias gc="git commit"
 alias gco="git checkout"
 alias gpo="git push origin"
+
+# peco
+function peco_select_history_order
+  if test (count $argv) = 0
+    set peco_flags --layout=top-down
+  else
+    set peco_flags --layout=bottom-up --query "$argv"
+  end
+
+  history|peco $peco_flags|read foo
+
+  if [ $foo ]
+    commandline $foo
+  else
+    commandline ''
+  end
+end
+
+function fish_user_key_bindings
+  bind /cr 'peco_select_history_order' # Ctrl + R
+end
---
+++ tmux/README.md
@@ -0,0 +1,7 @@
+# tmux
+
+tmux の設定ファイルの位置は、`~/.tmux.conf`なので、
+
+```
+cp -f tmux/.tmux.conf ~/.tmux.conf
+```
